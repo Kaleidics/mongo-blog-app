5ad6d98f64c31f615ddfe1922e059d2456e07804
Eddie Chu <contact@eddiechu.me>
2019-01-27 16:17:45 -0800

    update solution

--- server.js
+++ server.js
@@ -13,20 +13,7 @@ const app = express();
 app.use(morgan("common"));
 app.use(express.json());
 
-// app.get("/posts", function(req, res) {
-//     BlogPost
-//     .find()
-//     .then(function(posts){
-//         console.log(posts);
-//         res.json(posts.map(function(posts){
-//             posts.serialize()
-//         }));
-//     })
-//     .catch(function(error) {
-//         res.status(500).json({error: "encountered error"});
-//     });
-// });
-
+//all posts
 app.get('/posts', (req, res) => {
     BlogPost
         .find()
@@ -39,26 +26,93 @@ app.get('/posts', (req, res) => {
         });
 });
 
+// app.get("/posts", function(req, res) {
+//     BlogPost.find().then(function(posts) {
+//         res.json(posts.map(function(post) {
+//             post.serialize()
+//         }))
+//     .catch(function(error) {
+//         console.log(error);
+//         res.status(500).json({ error: 'failed'});
+//     });
+//     });
+// });
 
+//single post by id
+app.get("/posts/:id", (req, res) => {
+    BlogPost
+        .findById(req.params.id)
+        .then(post => res.json(post.serialize()))
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: "failed"})
+        });
+});
 
+//create a new post
+app.post("/posts", (req, res) => {
+    const requiredFields = ["title", "content", "author"];
+    for (let i=0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing ${field}`;
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
 
+    BlogPost
+        .create({
+            title: req.body.title,
+            content: req.body.content,
+            author: req.body.author
+        })
+        .then(blogPost => res.status(201).json(blogPost.serialize()))
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: "failed"});
+        });
+});
 
+//delete a post
+app.delete("/posts/:id", (req, res) => {
+    BlogPost.findByIdAndRemove(req.params.id)
+    .then(() => {
+        res.status(204).json({ message: "success"});
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: "failed"});
+    });
+});
 
+//update a post
+app.put("/posts/:id", (req, res) => {
+    if (!(req.params.id && req.body.id === req.body.id)) {
+        res.status(400).json({
+            error: "request path id and request body values must match"
+        });
+    }
 
+    const updated = {};
+    const updateableFields = ["title", "content", "author"];
+    updateableFields.forEach(field => {
+        if (field in req.body) {
+            updated[field] = req.body[field];
+        }
+    });
 
+    BlogPost
+        .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+        .then(updatedPost => res.status(204).end())
+        .catch(err => res.status(500).json({message: "failed"}));
 
+})
 
-
-
-
-
-
-
-
-
-
-
-
+//needs clarification
+app.use("*", function (req, res) {
+    res.status(404).json({ message: "Not found"});
+});
 
 
 let server;
